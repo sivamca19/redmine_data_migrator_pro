ad42432efbe01a5ee570b941f248074d6b2c45bf
sivamanikandan <siva.mca19@gmail.com>
2025-09-10 19:55:04 +0530

    added svg icon for menus

--- init.rb
+++ init.rb
@@ -9,11 +9,32 @@ Redmine::Plugin.register :redmine_data_migrator_pro do
   requires_redmine :version_or_higher => '4.0.0'
 
   menu :admin_menu, :data_migrator, { :controller => 'data_migrator', :action => 'index' },
-       :caption => 'Data Migrator',
+       :caption => %Q{
+    <svg xmlns="http://www.w3.org/2000/svg" class='s18 icon-svg' viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: middle; margin-right: 4px;">
+      <path d="M14 2H6a2 2 0 0 0-2 2v16a2 2 0 0 0 2 2h12a2 2 0 0 0 2-2V8z"></path>
+      <polyline points="14,2 14,8 20,8"></polyline>
+      <line x1="16" y1="13" x2="8" y2="13"></line>
+      <line x1="16" y1="17" x2="8" y2="17"></line>
+      <polyline points="10,9 9,9 8,9"></polyline>
+    </svg>
+    Data Migrator
+  }.html_safe,
        :html => { :class => 'icon icon-package' }
 
   menu :admin_menu, :external_asset_configs, { :controller => 'external_asset_configs', :action => 'index' },
-       :caption => 'External Asset Configs',
+       :caption => %Q{
+    <svg xmlns="http://www.w3.org/2000/svg" class='s18 icon-svg' viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" style="vertical-align: middle; margin-right: 4px;">
+      <rect x="3" y="4" width="18" height="18" rx="2" ry="2"></rect>
+      <line x1="9" y1="9" x2="15" y2="9"></line>
+      <line x1="9" y1="12" x2="15" y2="12"></line>
+      <line x1="9" y1="15" x2="15" y2="15"></line>
+      <circle cx="6" cy="9" r="1"></circle>
+      <circle cx="6" cy="12" r="1"></circle>
+      <circle cx="6" cy="15" r="1"></circle>
+      <path d="M21 9l-4 4-4-4"></path>
+    </svg>
+    External Asset Configs
+  }.html_safe,
        :html => { :class => 'icon icon-settings' }
 
   permission :manage_data_migration, { :data_migrator => [:index, :upload, :process, :history, :download_report] }
