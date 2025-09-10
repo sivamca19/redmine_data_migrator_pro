527ccc8293a3ed4db97b5bd6d756a464ee1a3c16
sivamanikandan <siva.mca19@gmail.com>
2025-09-10 15:24:27 +0530

    removed the index for new table

--- db/migrate/004_add_external_asset_config_to_data_migrations.rb
+++ db/migrate/004_add_external_asset_config_to_data_migrations.rb
@@ -1,6 +1,5 @@
 class AddExternalAssetConfigToDataMigrations < ActiveRecord::Migration[6.1]
   def change
-    add_reference :data_migrations, :external_asset_config, null: true, foreign_key: true
-    add_index :data_migrations, :external_asset_config_id
+    add_column :data_migrations, :external_asset_config_id, :integer
   end
 end
